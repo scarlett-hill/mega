dd72259fa44a79f78f5023020dff6c0d4199082b
mega <9108@protonmail.com>
2023-03-04 18:48:37 +0000

    :bug: launch configuration and wrong default value

--- src/sort.py
+++ src/sort.py
@@ -8,19 +8,19 @@ MAXIMUM_VALUE = 1_000_000
 
 
 def simulate():
-    length, generator = create_generator()
+    length, generator = create_generator(MAXIMUM_LENGTH, MAXIMUM_VALUE)
     ith = random.randint(0, length - 1)
     return find_i_th(generator, ith)
 
 
 def create_generator(
-    max_length: int = MAXIMUM_LENGTH, maximum_value: int = MAXIMUM_VALUE
+    max_length: int, maximum_value: int
 ) -> tuple[int, Generator[int, None, None]]:
     length = random.randint(1, max_length)
     return length, (random.randint(0, maximum_value) for _ in range(length))
 
 
-def find_i_th(generator: Generator[int, None, None], ith=int) -> float:
+def find_i_th(generator: Generator[int, None, None], ith: int) -> float:
     result = _find_i_th(list(generator), ith)
     logger.info(f"Here is the result: {result}.")
     return result
